c5890518bbde2b03ca4a00f9e956acecadc22255
Atsushi Uchida <uchida.lifeconnect@gmail.com>
2021-08-17 15:26:33 +0900

    書式整理
    - インデントを空白2つに統一
    - 不要な import を削除
    - ドキュメント全体をフォーマット（VSCode）

--- main.ts
+++ main.ts
@@ -1,81 +1,81 @@
 import * as CodeMirror from 'codemirror';
-import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, CachedMetadata, FileSystemAdapter } from 'obsidian';
-import {readdirSync, statSync} from "fs";
+import { App, MarkdownView, Modal, Plugin, PluginSettingTab, Setting, CachedMetadata, FileSystemAdapter } from 'obsidian';
+import { readdirSync, statSync } from "fs";
 import * as path from 'path';
 
 type GetSettings = (
-	data: CachedMetadata,
-	cursor: CodeMirror.Position
+  data: CachedMetadata,
+  cursor: CodeMirror.Position
 ) => MyPluginSettings;
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-	public settings: MyPluginSettings = DEFAULT_SETTINGS;
+  public settings: MyPluginSettings = DEFAULT_SETTINGS;
 
-	async onload() {
-		console.log('loading plugin');
+  async onload() {
+    console.log('loading plugin');
 
-		await this.loadSettings();
+    await this.loadSettings();
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+    this.addCommand({
+      id: 'open-sample-modal',
+      name: 'Open Sample Modal',
+      // callback: () => {
+      // 	console.log('Simple Callback');
+      // },
+      checkCallback: (checking: boolean) => {
+        let leaf = this.app.workspace.activeLeaf;
+        if (leaf) {
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
+          return true;
+        }
+        return false;
+      }
+    });
 
-		// コマンド
-		this.addCommand({
-			id: 'add-toc-notes',
-			name: 'Add Sample TOC Notes',
-			callback: this.createTocForActiveFile(),
-		});
+    // コマンド
+    this.addCommand({
+      id: 'add-toc-notes',
+      name: 'Add Sample TOC Notes',
+      callback: this.createTocForActiveFile(),
+    });
 
-		// 設定内容
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-	}
+    // 設定内容
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+  }
 
-	private createTocForActiveFile = (
-		settings: MyPluginSettings | GetSettings = this.settings
+  private createTocForActiveFile = (
+    settings: MyPluginSettings | GetSettings = this.settings
   ) => () => {
     const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
     if (activeView && activeView.file) {
-			const cursor = activeView.editor.getCursor();
+      const cursor = activeView.editor.getCursor();
       const data = this.app.metadataCache.getFileCache(activeView.file) || {};
-			const activeFile = activeView.file;
-			const relativePath = activeFile.parent.path;
-			if (this.app.vault.adapter instanceof FileSystemAdapter) {
-      	var absolutePath = path.join(this.app.vault.adapter.getBasePath(), relativePath);
-			} else {
-      	var absolutePath = path.join(this.app.vault.getResourcePath(activeFile), relativePath);
-			}
-			console.log(absolutePath);
+      const activeFile = activeView.file;
+      const relativePath = activeFile.parent.path;
+      if (this.app.vault.adapter instanceof FileSystemAdapter) {
+        var absolutePath = path.join(this.app.vault.adapter.getBasePath(), relativePath);
+      } else {
+        var absolutePath = path.join(this.app.vault.getResourcePath(activeFile), relativePath);
+      }
+      console.log(absolutePath);
 
-			var toc = "";
-			var c = 1;
-			const arr = this.getMarkdownFileList(absolutePath);
-			arr.forEach(v => {
-				toc += c + ". " + "[[" + v.name + "]]" + "\n";
-				c++;
-			});
+      var toc = "";
+      var c = 1;
+      const arr = this.getMarkdownFileList(absolutePath);
+      arr.forEach(v => {
+        toc += c + ". " + "[[" + v.name + "]]" + "\n";
+        c++;
+      });
 
       if (toc) {
         activeView.editor.replaceRange(toc, cursor);
@@ -83,80 +83,80 @@ export default class MyPlugin extends Plugin {
     }
   };
 
-	private getMarkdownFileList(dirPath:string) {
-			let fileList = readdirSync(dirPath, {
-					withFileTypes: true, 
-			})
-			.filter(dirent => dirent.isFile())
-			.filter(dirent => dirent.name.endsWith(".md"))
-			.map(dirent => {
-				return {
-					name: dirent.name,
-					ctime: statSync(path.join(dirPath, dirent.name)).ctime,
-					mtime: statSync(path.join(dirPath, dirent.name)).mtime,
-				}
-			});
-			// ソートして返却
-			// fileList.sort((a, b) => b.mtime.getMilliseconds() - a.mtime.getMilliseconds());
-			fileList.sort((a, b) => b.ctime.getTime() - a.ctime.getTime());
+  private getMarkdownFileList(dirPath: string) {
+    let fileList = readdirSync(dirPath, {
+      withFileTypes: true,
+    })
+      .filter(dirent => dirent.isFile())
+      .filter(dirent => dirent.name.endsWith(".md"))
+      .map(dirent => {
+        return {
+          name: dirent.name,
+          ctime: statSync(path.join(dirPath, dirent.name)).ctime,
+          mtime: statSync(path.join(dirPath, dirent.name)).mtime,
+        }
+      });
+    // ソートして返却
+    // fileList.sort((a, b) => b.mtime.getMilliseconds() - a.mtime.getMilliseconds());
+    fileList.sort((a, b) => b.ctime.getTime() - a.ctime.getTime());
 
-			return fileList;
-	}
+    return fileList;
+  }
 
-	onunload() {
-		console.log('unloading plugin');
-	}
+  onunload() {
+    console.log('unloading plugin');
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onOpen() {
+    let { contentEl } = this;
+    contentEl.setText('Woah!');
+  }
 
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Table of Contents Note - Settings'});
+    containerEl.createEl('h2', { text: 'Table of Contents Note - Settings' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue('')
+        .onChange(async (value) => {
+          console.log('Secret: ' + value);
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
